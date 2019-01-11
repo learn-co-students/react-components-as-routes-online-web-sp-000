5aef320b936471151c4f37d50e1fea4bd3ca70d9
danmj <daniel.jung@stern.nyu.edu>
2019-01-11 17:15:33 -0500

    d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+// .src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,7 +13,40 @@ const Home = () => {
   );
 };
 
-ReactDOM.render(
-  <Home />,
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+
+// Step 2. Changed to have router coordinate what is displayed
+ReactDOM.render((
+  <Router>
+      <Route path="/" render={Home} />
+      <Route exact path="/about" render={About} />
+      <Route exact path="/login" render={Login} />
+  </Router>),
   document.getElementById('root')
 );
