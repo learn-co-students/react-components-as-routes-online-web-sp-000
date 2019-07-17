5b83bb30b3863240c09bd395f40ef5b23587b873
Adam Robinson <luohaoxiang@yahoo.com>
2019-07-16 21:24:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home.js'
+import Login from './Login.js'
+import About from './About.js'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
 
-ReactDOM.render(
-  <Home />,
+
+// componenent is for class componenets where as render is for functional componenets
+
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment> 
+    <Route path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/login" component={Login} />
+    </React.Fragment>
+  </Router>
+  ),
   document.getElementById('root')
 );
